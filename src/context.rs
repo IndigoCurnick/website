a9983306bb67f50df6d9ad229e22cf3b963e637b
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2023-07-29 15:04:41 +0100

    just making it run with the new blog stuff

--- src/context.rs
+++ src/context.rs
@@ -6,5 +6,5 @@ use lazy_static::lazy_static;
 pub static BLOG_ROOT: &str = "blog";
 
 lazy_static! {
-    pub static ref STATIC_BLOG_ENTRIES: Blog = get_blog_entries(PathBuf::from(BLOG_ROOT));
+    pub static ref STATIC_BLOG_ENTRIES: Blog = get_blog_entries(PathBuf::from(BLOG_ROOT), None);
 }
