1bef4aa7a832cc6ae5a77c62e47c3857ad85f341
Nathaniel Curnick <nathaniel.curnick@gmail.com>
2023-11-18 08:03:40 +0000

    added toolbox links

--- src/routes/toolbox/mod.rs
+++ src/routes/toolbox/mod.rs
@@ -6,16 +6,21 @@ use crate::{database::insert_to_database, DOMAIN};
 #[get("/toolbox/geodesics")]
 pub async fn geodesics() -> Template {
     tokio::spawn(async move {
-        insert_to_database(
-            DOMAIN.to_string(),
-            "/courses/kalman-filters/sin-wave".to_string(),
-        )
-        .await;
+        insert_to_database(DOMAIN.to_string(), "/toolbox/geodesics".to_string()).await;
     });
     let context = rocket_dyn_templates::tera::Context::new();
     Template::render("toolbox/geodesics", context.into_json())
 }
 
+#[get("/toolbox")]
+pub async fn index() -> Template {
+    tokio::spawn(async move {
+        insert_to_database(DOMAIN.to_string(), "/toolbox".to_string()).await;
+    });
+    let context = rocket_dyn_templates::tera::Context::new();
+    Template::render("toolbox/index", context.into_json())
+}
+
 pub fn get_toolbox_routes() -> Vec<Route> {
-    return routes![geodesics];
+    return routes![geodesics, index];
 }
